b2f249d36a2f823b5c5a23b0a17dd882a1c16cea
jairo5200 <jairo5200@gmail.com>
2025-02-27 13:18:38 -0500

    implementamos login

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,34 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 # Create your views here.
-def helloworld(request):
-    return HttpResponse("Hello, world!")
+def home(request):
+    return render(request,'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request,'signup.html',{
+            'form':UserCreationForm()
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                #registra un user
+                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+                user.save()
+                return redirect('tasks')
+            except:
+                return render(request,'signup.html',{
+                    'form':UserCreationForm(),
+                    'error':'Username already exists'
+                })
+        return render(request,'signup.html',{
+            'form':UserCreationForm(),
+            'error':'Passwords do not match'
+        })
+    
+def tasks(request):
+    return render(request,'tasks.html')
