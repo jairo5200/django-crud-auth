77f5b1abb65cdc85e384546c51c4753b9bb367fd
jairo5200 <jairo5200@gmail.com>
2025-02-27 14:04:52 -0500

    implementamos un login

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout,authenticate
 
 # Create your views here.
 def home(request):
@@ -19,6 +19,7 @@ def signup(request):
                 #registra un user
                 user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
                 user.save()
+                login(request,user)
                 return redirect('tasks')
             except:
                 return render(request,'signup.html',{
@@ -32,3 +33,23 @@ def signup(request):
     
 def tasks(request):
     return render(request,'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request,'signin.html',{
+            'form':AuthenticationForm()
+        })
+    else:
+        user = authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            login(request,user)
+            return redirect('tasks')
+        return render(request,'signin.html',{
+            'form':AuthenticationForm(),
+            'error':'Invalid username or password'
+            })
+    
